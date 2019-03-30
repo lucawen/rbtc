3389c9874027d866e9321898a1fd42e5ac4c2176
Luca Lacerda <lucalacerda1@gmail.com>
2019-03-30 14:58:50 -0300

    Create opt args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use] extern crate serde_derive;
-use std::{env, process::exit};
+use std::process::exit;
 use reqwest;
+use structopt::StructOpt;
 
 const API_URL: &'static str = "https://apiv2.bitcoinaverage.com/convert/global";
 
@@ -11,22 +12,32 @@ struct BtcResponse {
     price: f64
 }
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rbtc", about = "Get value of a btc value to a currency")]
+struct Opt {
+    /// Set amount to convert to a currency or from a currency
+    amount: f64,
+    /// Set the initial currency of
+    #[structopt(short = "f", long = "from", default_value = "BTC")]
+    from: String,
+    /// Set the final currency to convert
+    #[structopt(short = "t", long = "to",  default_value = "USD")]
+    to: String,
+    /// Verbose information not will displayed
+    #[structopt(short = "s", long = "silent")]
+    silent: bool,
+}
+
 fn main() {
-    let amount = match env::args().nth(1) {
-        Some(value) => value,
-        None => {
-            eprintln!("Expected btc amount. Exiting.");
-            exit(1);
-        }
-    };
-    let currency = env::args().nth(2).unwrap_or(String::from("USD"));
+    let opt = Opt::from_args();
     let client  = reqwest::Client::new();
+    let options_req = [
+        ("from", &opt.from),
+        ("to", &opt.to), 
+        ("amount", &opt.amount.to_string()),  
+    ];
     let request = client.get(API_URL)
-        .query(&[
-            ("from", "BTC"),  
-            ("to", &currency),  
-            ("amount", &amount),  
-        ]);
+        .query(&options_req);
     let response_result = request.send();
 
     let btc_response = match response_result {
@@ -48,22 +59,9 @@ fn main() {
         eprintln!("Error ocurred in conversion");
         exit(1);
     }
-    println!("The conversion of {} BTC to {} is {}", amount, currency, btc_response.price);
-}
-
-#[test]
-fn test_get_amount() {
-    let good_input = String::from("123.456");
-    assert_eq!(get_amount(good_input), 123.456);
-}
-
-#[test]
-#[should_panic]
-fn test_get_amount_bad() {
-    let badinput = String::from("no soy btc");
-    get_amount(badinput);
-}
-
-fn get_amount(input: String) -> f64 {
-    input.parse().expect("Failed to parse string")
+    if opt.silent {
+        println!("{}", btc_response.price);
+    } else {
+        println!("{} {}", btc_response.price, &opt.to); 
+    }
 }
